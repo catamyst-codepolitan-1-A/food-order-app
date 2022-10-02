831bc1d8571d60cdd2d5f1fc9f3f3d2dcaaa32ea
Agung Nur Ramadhan <agnuramdan@gmail.com>
2022-10-02 12:06:21 +0700

    feat : add update total cart function refactor : changes variables name to be more explanable

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ for (let addButton = 0; addButton < addToCartButton.length; addButton++) {
 // add item to cart
 function addToCartClick(event) {
   const button = event.target;
-  const shopItem = button.parentElement.parentElement.parentElement;
-  const foodName = shopItem.getElementsByClassName("food-name")[0].innerText;
-  const foodPrice = shopItem.getElementsByClassName("food-price")[0].innerText;
-  const foodImage = shopItem.getElementsByClassName("food-image")[0].src;
+  const foodItem = button.parentElement.parentElement.parentElement;
+  const foodName = foodItem.getElementsByClassName("food-name")[0].innerText;
+  const foodPrice = foodItem.getElementsByClassName("food-price")[0].innerText;
+  const foodImage = foodItem.getElementsByClassName("food-image")[0].src;
   addItemToCart(foodName, foodPrice, foodImage);
-  // console.log(shopItem);
+  // console.log(foodItem);
   // console.log(foodName, foodPrice, foodImage);
 }
 
@@ -39,13 +39,55 @@ function addItemToCart(foodName, foodPrice, foodImage) {
         <img width="100" src="${foodImage}" alt="image" />
         <h4 class="cart-item-name">${foodName}</h4>
       </div>
-      <h4>${foodPrice}</h4>
+      <h4 class="cart-food-price">${foodPrice}</h4>
       <div class="cart-quantity">
         <i class="las la-minus"></i>
-        <h4>1</h4>
+        <h4 class="cart-item-quantity">1</h4>
         <i class="las la-plus"></i>
       </div>
       <i class="las la-trash"></i>
     </div>`;
   cartItems.append(cartRow);
+  removeFromCartButton();
+  updateCartTotal();
+}
+
+// remove cart button function
+
+function removeFromCartButton() {
+  const removeItemButton = document.getElementsByClassName("la-trash");
+  for (
+    let removeButton = 0;
+    removeButton < removeItemButton.length;
+    removeButton++
+  ) {
+    const button = removeItemButton[removeButton];
+    // console.log(button);
+    button.addEventListener("click", (event) => {
+      const buttonClicked = event.target;
+      button.parentElement.remove();
+      updateCartTotal();
+    });
+  }
+}
+
+// update total cart
+function updateCartTotal() {
+  const cartItemContainer = document.getElementsByClassName("cart-row")[0];
+  const cartItem = cartItemContainer.getElementsByClassName("cart-item");
+  let totalPrice = 0;
+  for (let cartUpdate = 0; cartUpdate < cartItem.length; cartUpdate++) {
+    const cartElement = cartItem[cartUpdate];
+    const cartPriceElement =
+      cartElement.getElementsByClassName("cart-food-price")[0];
+    const cartQuantityElement =
+      cartElement.getElementsByClassName("cart-item-quantity")[0];
+    cartPrice = cartPriceElement.innerText.replace("IDR", "");
+    cartQuantity = cartQuantityElement.innerText;
+    totalPrice = totalPrice + cartPrice * cartQuantity;
+  }
+  console.log(cartPrice * cartQuantity);
+  document.getElementsByClassName(
+    "cart-total-price"
+  )[0].innerText = `IDR ${totalPrice}`;
 }
